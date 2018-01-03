e725941abfe86c473d38d18f587e757f09963a04
Jarvis Potter <jarviskpotter@gmail.com>
2018-01-03 18:56:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ['snickers', 'hundered grand', 'kitkat', 'skittles']
+
+
+function addElementToBeginningOfArray(javascript, node){
+  var myArray = ['jarvis', ...]
+  return
+}
+
+function destructivelyAddElementToBeginningOfArray(javascript, node){
+  return myArray
+}
